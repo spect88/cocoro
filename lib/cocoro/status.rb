053514b77022228f2ed45491efbb176ebddf41bc
Tomasz Szczęśniak-Szlagowski <spect88@gmail.com>
2022-01-08 23:27:23 +0900

    Fix PM2.5 and remove unnecessary fields

--- lib/cocoro/status.rb
+++ lib/cocoro/status.rb
@@ -9,23 +9,21 @@ module Cocoro
       temperature: ["F1", 4, 1], # °C
       humidity: ["F1", 5, 1], # %
       total_air_cleaned: ["F1", 22, 4], # m^3
-      pm25: ["F1", 28, 2], # ug/m^3 : sometimes weird values because of the first byte
-      f2unknown1: ["F2", 1, 1],
+      pm25: ["F1", 28, 2], # ug/m^3, only last 9 bits are important
       odor: ["F2", 15, 1], # 0/33/66/100 (100=dirty)
       dust: ["F2", 16, 1], # 0/25/50/75/100 (100=dirty)
       overall_dirtiness: ["F2", 18, 1], # 0/25/50/75/100 (100=dirty)
-      f2unknown19: ["F2", 19, 1],
       enough_water: ["F2", 20, 1],
       light_detected: ["F2", 21, 1],
       air_volume: ["F3", 5, 1],
       power_on: ["F3", 14, 1],
-      humidifier_on: ["F3", 16, 1],
-      f3unknown20: ["F3", 20, 1]
+      humidifier_on: ["F3", 16, 1]
     }.freeze
     VALUES = {
       enough_water: { 0 => false, 255 => true },
       light_detected: { 0 => false, 255 => true },
       air_volume: {
+        0 => nil,
         16 => "auto",
         17 => "night",
         19 => "pollen",
@@ -56,25 +54,15 @@ module Cocoro
       define_method(field) { read_value(field) }
     end
 
-    # Get other data that we're not sure what it means, but could be useful for debugging
-    def unknowns
-      LOCATIONS
-        .keys
-        .select { |key| key.to_s =~ /unknown/ }
-        .map { |key| [key, read_value(key)] }
+    def to_h
+      (BOOLEAN_FIELDS + NON_BOOLEAN_FIELDS)
+        .map { |f| [f, read_value(f)] }
         .to_h
         .merge(
           f1: read_binary_status("F1"),
           f2: read_binary_status("F2"),
           f3: read_binary_status("F3")
         )
-    end
-
-    def to_h
-      (BOOLEAN_FIELDS + NON_BOOLEAN_FIELDS)
-        .map { |f| [f, read_value(f)] }
-        .to_h
-        .merge(unknowns)
     end
 
     protected
@@ -87,6 +75,8 @@ module Cocoro
       number = hex_code[(byte_offset - 1) * 2, byte_length * 2].to_i(16)
       if VALUES[name]&.key?(number)
         VALUES[name][number]
+      elsif name == :pm25
+        number & ((2**9) - 1) # last 9 bits only
       else
         number
       end
--- spec/cocoro/status_spec.rb
+++ spec/cocoro/status_spec.rb
@@ -9,35 +9,60 @@ RSpec.describe Cocoro::Status do
         "valueType" => "valueBinary",
         "valueSingle" => nil,
         "valueRange" => nil,
-        "valueBinary" => {
-          "code" => <<~HEX.gsub(/\s+/, "")
-            6201D8163002FE00F0000000001C760078000000790000346A000000000000000000000005050008
-          HEX
-        }
+        "valueBinary" => { "code" => f1 }
       },
       {
         "statusCode" => "F2",
         "valueType" => "valueBinary",
         "valueSingle" => nil,
         "valueRange" => nil,
-        "valueBinary" => {
-          "code" => <<~HEX.gsub(/\s+/, "")
-            00600000000000000000000000000000000000FFFF00000081FF0014010000000000000000000000
-          HEX
-        }
+        "valueBinary" => { "code" => f2 }
       },
       {
         "statusCode" => "F3",
         "valueType" => "valueBinary",
         "valueSingle" => nil,
         "valueRange" => nil,
-        "valueBinary" => {
-          "code" => <<~HEX.gsub(/\s+/, "")
-            00000000140000008000000000FF00FF0000000000000000000000
-          HEX
-        }
+        "valueBinary" => { "code" => f3 }
       }
     ]
+  end
+  let(:f1) do
+    [
+      "6201D8", # 1-3: unknown
+      "16", # 4: temperature
+      "30", # 5: humidity
+      "02FE00F0000000001C76007800000079", # 6-21: unknown
+      "0000346A", # 22-25: total air cleaned
+      "0000", # 26-27: unknown
+      pm25_bytes, # 28-29: pm25 (only last 9 bits)
+      "0000000000000005050008" # 30-40: unknown
+    ].join
+  end
+  let(:pm25_bytes) { "0000" }
+  let(:f2) do
+    [
+      "0060000000000000000000000000", # 1-14: unknown
+      "00", # 15: odor
+      "00", # 16: dust
+      "00", # 17: unknown
+      "00", # 18: overall dirtiness
+      "00", # 19: unknown
+      "FF", # 20: enough water
+      "FF", # 21: light detected
+      "00000081FF0014010000000000000000000000" # 22-40: unknown
+    ].join
+  end
+  let(:f3) do
+    [
+      "00000000", # 1-4: unknown
+      "14", # 5: air volume (aka mode)
+      "0000008000000000", # 6-13: unknown
+      "FF", # 14: power on
+      "00", # 15: unknown
+      "FF", # 16: humidifier on
+      "0000000000000000000000" # 17-27: unknown
+    ].join
   end
 
   its(:enough_water?) { is_expected.to eq(true) }
@@ -55,4 +80,12 @@ RSpec.describe Cocoro::Status do
   its(:air_volume) { is_expected.to eq("quiet") }
 
   its(:to_h) { is_expected.to include(:power_on, :pm25, :air_volume, :f1) }
+
+  context "when the hex value for PM2.5 is larger than 512" do
+    let(:pm25_bytes) { "8001" }
+
+    it "only takes the last 9 bits (up to 511)" do
+      expect(status.pm25).to eq(1)
+    end
+  end
 end
